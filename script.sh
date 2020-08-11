b51d735f7b06b3c2b29124a7cc73d6952feeff99
Kéwan Bernier <mail@kewan.fr>
2020-08-11 21:11:57 +0200

    Edit Script et sql

--- script.sh
+++ script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo -e "Script lancé ! \n"
-cd /opt/databasess
+cd /opt/databases
 sudo -i
 sudo mysql -u "root" < "CCC.sql"
 echo -e "Import Database réussi ! \n"
+cd /opt/databases
+
